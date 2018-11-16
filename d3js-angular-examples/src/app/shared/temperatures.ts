466fcec5798e8c8c98f8e38383d38716e57f9d4d
Anmol Chawla <33879579+anmolchawla@users.noreply.github.com>
2018-11-15 20:47:58 -0800

    Angular Framework
    Angular framework working, Uber api working, Cards working, Need to add Js Visuals.

---
+++ d3js-angular-examples/src/app/shared/temperatures.ts
@@ -0,0 +1,18 @@
+export const TEMPERATURES: any[] = [
+{"name":"Country",
+ "children":[
+{"name":"China","size":42.04},
+{"name": "France","size":19.82},
+{"name":"India","size":66.4},
+{"name":"Portugal","size":35.348},
+{"name":"Spain","size":19.92},
+{"name":"Srilanka","size":81.616},
+{"name":"Sweden","size":12.854},
+{"name":"Switzerland","size":26.239},
+{"name":"U.K","size":16.857},
+{"name":"U.S","size":17.942}
+]
+}
+];
+
+
