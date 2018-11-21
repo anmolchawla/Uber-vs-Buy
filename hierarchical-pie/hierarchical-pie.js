94c26b84520ef3d5f2ef86c5a8ab1189982fb8e3
David Goodfellow <dgoodfel@usc.edu>
2018-11-20 19:51:38 -0800

    Update hierarchical-pie.js

--- hierarchical-pie/hierarchical-pie.js
+++ hierarchical-pie/hierarchical-pie.js
@@ -1,211 +1,127 @@
-  /*global $:false ko:false console:false d3:false */
-var HierarchicalPie = function(options) {
-  var self = this;
+d3.json('data_pie.json').then(function(data_before){
+    //Setting up variables
+    var inLevel = 1;
+    var dataChain = [];
+    var choiceGlobal = 0;
+    var maxV = 0
+    var firstData = data_before.map(x => x);
+    var radius = Math.min(document.getElementById('chart1').clientWidth-1,document.getElementById('chart1').clientHeight-1)/2
+    dataChain.push(firstData);
 
-  var config = {
-    width             : 800,
-    height            : 500,
-    chartId           : null,
-    data              : null,
-    legendContainer   : null,
-    hoverRadiusDiff   : 10,
-    navigation        : null,
-    hideNavOnRoot     : true,
-    dataSchema        : {
-      categoryField : 'category',
-      idField       : 'id_category',
-      valueField    : 'cost',
-      childrenField : 'categories'
-    },
-    hoverPieAnimation : {
-      easing   : "elastic",
-      duration : 1000
-    },
-    focusAnimation : {
-      easing   : "easeInOutQuart",
-      duration : 100
-    }
-  };
+    //set init variables
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+    var arc = d3.arc()
+        .outerRadius(radius - 10)
+        .innerRadius(radius / 2);
+    var arcOver = d3.arc()
+        .outerRadius(radius)
+        .innerRadius(radius / 2);
+    var pie = d3.pie()
+        .sort(null)
+        .value(function(d) {
+            var val = d.cost;
+            if(typeof val === 'string')
+                return parseFloat(val);
+            return val;
+      });
 
-  $.extend(config, config, options || {});
+    //add main svg
+    var svg = d3.select("#chart1")
+        .append("g")
+        .attr("transform", "translate(" + ((document.getElementById('chart1').clientWidth-1)/2) + "," + ((document.getElementById('chart1').clientHeight-1)/2) + ")");;
+    
+    //inner labeling for 
+    var focusGroup = svg.append('g')
+        .attr('class', 'focus-group');
+    var nameLabel = focusGroup.append('g')
+        .attr('class', 'arc-percent')
+        .append("text")
+        .attr("dy", "-1.2em");
+    var percentLabel = focusGroup.append('g')
+        .attr('class', 'arc-percent')
+        .append("text");
+    var costLabel = focusGroup.append('g')
+        .attr('class', 'arc-cost')
+        .append("text")
+        .attr("dy", "1.2em");
 
-  this.tweenPie = function(b){
-    b.innerRadius = 0;
-    var i = d3.interpolate({startAngle: 0, endAngle: 0}, b);
-    return function(t) { return self.arc(i(t)); };
-  };
+    //set button actions
+    d3.select("#resetButton")
+        .on("click", goLevelUp)
+    
+    renderPie(firstData)
 
-  this.tabulateCategories = function (data) {
-    var table = d3.select(config.legendContainer).select('table');
-    table.select('tbody').remove();
-    var tbody = table.append('tbody');
-    var rowTemplate = Mustache.compile($('#rowTemplate').html());
-    // create a row for each object in the data
-    var tableRows = tbody.selectAll("tr")
-        .data(data)
-        .enter()
-          .append("tr")
-          .attr('data_id', function(d) { return d[config.dataSchema.idField]; })
-          .attr('class', function(d) { return 'legend-row-' + d[config.dataSchema.idField]; })
-          .html(function(d) {
-            d.color = self.color(d[config.dataSchema.idField]);
-            d.isDirect = d[config.dataSchema.idField] == null;
-            return rowTemplate(d);
-          });
-
-    return table;
-  };
-
-  // chart width
-  self.width  = config.width;
-  self.height = config.height;
-  // pie radius
-  self.radius = Math.min(self.width, self.height) / 2;
-  self.inLevel = 1;
-  //data for each level of chart, to make navigation possible
-  self.dataChain = [];
-
-  this.init = function() {
-    self.palette = d3.scale.ordinal()
-      .range(['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#8f7540', '#bcbd22', '#17becf', '#d7de85', '#754a5f', '#857c57', '#46a2b0', '#ff9896']);
-    self.color = function(id) {
-      return id === null ? '#ddd' : self.palette(id);
+    function goLevelUp() {
+        if(inLevel == 1){
+            return false;
+        }
+        inLevel = inLevel-1;
+        dataChain.splice(dataChain.length - 1, 1);
+        var prev = dataChain[dataChain.length - 1];
+        console.log(prev)
+        renderPie(prev);
+        if(inLevel === 1)
+            console.log("first layer")  
+        return false;
     };
-    self.arc     = d3.svg.arc().outerRadius(self.radius - config.hoverRadiusDiff).innerRadius(self.radius / 2);
-    self.arcOver = d3.svg.arc().outerRadius(self.radius).innerRadius(self.radius / 2);
-    self.pie     = d3.layout.pie().sort(null).value(function(d) {
-      var val = d[config.dataSchema.valueField];
-      if(typeof val === 'string')
-        return parseFloat(val);
 
-      return val;
-    });
+    function sliceClick(d, i) {
+        console.log("click")
+        console.log(d)
+        if(typeof d.data.categories === 'undefined')
+          return false;
+        inLevel++;
+        dataChain.push( d.data.categories );
+        renderPie(d.data.categories);
+        return false;
+    };
 
-    self.svg = d3.select(config.chartId).append("svg")
-      .attr('id', 'chart').attr("width", self.width)
-      .attr("height", self.height)
-      .append("g")
-        .attr("transform", "translate(" + self.radius + "," + ((self.height / 2)) + ")");
+    function pieMouseOut(d, i) {
+        var hovered = d3.select(this);   
+        focusGroup.transition().attr('opacity', 0);
+        hovered.transition()
+            //.ease("easeInOutQuart")
+            .duration(100)
+            .attr("d", arc);
+    };
 
-    self.focusGroup = self.svg.append('g').attr('class', 'focus-group');
+    function pieMouseOver(d, i) {
+        var hovered = d3.select(this);
+        var percentage = (((d.endAngle - d.startAngle) / (2 * Math.PI)) * 100).toFixed(1);
+        nameLabel.text(d.data.category);
+        percentLabel.text(percentage + '%');
+        costLabel.text(d.data.cost);
+        focusGroup.transition().attr('opacity', 1);
+        hovered.transition()
+            //.ease("easeInOutQuart")
+            .duration(100)
+            .attr("d", arcOver);
+    };
 
-    self.nameLabel = self.focusGroup.append('g')
-      .attr('class', 'arc-percent').append("text").attr("dy", "-1.2em");
+    function tweenPie(b){
+        b.innerRadius = 0;
+        var i = d3.interpolate({startAngle: 0, endAngle: 0}, b);
+        return function(t) { return arc(i(t)); };
+    };
 
-    self.percentLabel = self.focusGroup.append('g')
-      .attr('class', 'arc-percent').append("text");
-
-    self.costLabel = self.focusGroup.append('g')
-      .attr('class', 'arc-cost').append("text").attr("dy", "1.2em");
-
-    self.navigation = $(config.navigation);
-    self.navigation.find('#btnRoot').on('click', self.goToRoot);
-    self.navigation.find('#btnLevelUp').on('click', self.goLevelUp);
-
-    if(!config.hideNavOnRoot)
-      self.navigation.show();
-  };
-
-  this.goToRoot = function() {
-    self.inLevel = 1;
-    self.dataChain.length = 0; //clear chain
-    self.renderCake(config.data);
-
-    if(config.hideNavOnRoot)
-      self.navigation.hide();
-
-    return false;
-  };
-
-  this.goLevelUp = function() {
-    if(self.inLevel == 2 || self.dataChain.length == 0)
-      return self.goToRoot();
-
-    self.inLevel--;
-    self.dataChain.splice(self.dataChain.length - 1, 1);
-    var prev = self.dataChain[self.dataChain.length - 1];
-    self.renderCake(prev[config.dataSchema.childrenField]);
-    if(self.inLevel === 1 && config.hideNavOnRoot)
-      self.navigation.hide();
-
-    return false;
-  };
-
-  this.updateNav = function() {
-    var breadcrumb = d3.select(config.navigation).select('.breadcrumb');
-    breadcrumb.selectAll('li').remove();
-    breadcrumb.selectAll('li')
-      .data(self.dataChain)
-      .enter()
-        .append("li")
-        .html(function(d, i) {
-          return d.category + '<span class="divider">/</span>';
-        });
-  }
-
-  this.pieClick = function (d, i) {
-    var c = self.arc.centroid(d);
-    if(typeof d.data[config.dataSchema.childrenField] === 'undefined')
-      return false;
-
-    //console.log(d.data);
-    self.inLevel++;
-    self.dataChain.push( d.data );
-
-    self.focusGroup.attr('opacity', 0);
-    d3.select(this).attr("d", self.arc);
-
-    self.renderCake(d.data[config.dataSchema.childrenField]);
-    if(self.inLevel > 1)
-      self.navigation.show();
-
-    return false;
-  };
-
-  this.pieMouseOut = function (d, i) {
-    var hovered = d3.select(this);
-
-    self.focusGroup.transition().attr('opacity', 0);
-    hovered.transition().ease(config.focusAnimation.easing).duration(config.focusAnimation.duration).attr("d", self.arc);
-
-    d3.select(config.legendContainer).select('.legend-row-' + d.data[config.dataSchema.idField]).selectAll('td').classed("hovered", false);
-  };
-
-  this.pieMouseOver = function (d, i) {
-    var hovered = d3.select(this);
-
-    var percentage = (((d.endAngle - d.startAngle) / (2 * Math.PI)) * 100).toFixed(1);
-    self.nameLabel.text(d.data[config.dataSchema.categoryField]);
-    self.percentLabel.text(percentage + '%');
-    self.costLabel.text(d.data[config.dataSchema.valueField]);
-    self.focusGroup.transition().attr('opacity', 1);
-    hovered.transition().ease(config.focusAnimation.easing).duration(config.focusAnimation.duration).attr("d", self.arcOver);
-
-    d3.select(config.legendContainer).select('.legend-row-' + d.data[config.dataSchema.idField]).selectAll('td').classed("hovered", true);
-  };
-
-  this.renderCake = function(data) {
-    self.updateNav();
-    self.svg.select('g.cake').remove();
-    var arcs = self.svg.append('g').attr('class', 'cake')
-      .selectAll("g.arc").data(self.pie(data))
-      .enter().append("g")
-        .attr("class", "arc");
-
-    arcs.append("path").attr("d", self.arc)
-      .attr("fill", function(d) { return self.color(d.data[config.dataSchema.idField]); })
-      .attr("stroke", function(d) { return d3.rgb(self.color(d.data[config.dataSchema.idField])).darker(); })
-      .attr('class', function(d) { return 'category-pie-' + d.data[config.dataSchema.idField] + (typeof d.data[config.dataSchema.childrenField] === 'undefined' ? ' pie-leaf' : ''); })
-      .on('mouseover', self.pieMouseOver)
-      .on('mouseout', self.pieMouseOut)
-      .on('click', self.pieClick)
-      .transition().ease(config.hoverPieAnimation.easing).duration(config.hoverPieAnimation.duration)
-      .attrTween("d", self.tweenPie);
-
-    self.tabulateCategories(data);
-  };
-
-  self.init();
-  self.renderCake(config.data);
-
-};
+    function renderPie(data) {
+        svg.select('g.cake').remove();
+        var arcs = svg.append('g')
+            .attr('class', 'cake')
+            .selectAll("g.arc").data(pie(data))
+            .enter().append("g")
+            .attr("class", "arc");
+    
+        arcs.append("path").attr("d", arc)
+            .attr("fill", function(d,i) { return color(i); })
+            .attr("stroke", function(d,i) { return d3.rgb(color(i)).darker(); })
+            .attr('class', function(d,i) { return 'category-pie-' + i + (typeof d.categories === 'undefined' ? ' pie-leaf' : ''); })
+            .on('mouseover', pieMouseOver)
+            .on('mouseout', pieMouseOut)
+            .on('click', sliceClick)
+            .transition()
+            //.ease("easeInOutQuart")
+            .duration(1000)
+            .attrTween("d", tweenPie);
+      };
+});
